e646598ef2429d3fad1fdc661182c0147ca81381
Murii <muresanvladmihail@gmail.com>
2017-08-23 16:06:14 +0300

    lists

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 
+/*
+ * Big thanks for helping me out to: BitPuffin http://bitpuffin.com
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define car(p) ((p).pair->atom[0])
 #define cdr(p) ((p).pair->atom[1])
@@ -33,6 +38,12 @@ struct Atom
 
 };
 
+typedef enum
+{
+	Error_OK = 0,
+	Error_Syntax = 1
+} Error;
+
 struct Pair
 {
 	struct Atom atom[2];
@@ -41,6 +52,7 @@ struct Pair
 typedef struct Atom Atom;
 
 static const Atom nil = { AtomType_Nil };
+static Atom sym_table = { AtomType_Nil };
 
 /* Allocate a pair on the heap and assign its two elements. */
 static Atom cons(Atom car_value, Atom cdr_value)
@@ -67,11 +79,204 @@ static Atom make_int(long value)
 
 static Atom make_symbol(const char* value)
 {
-	Atom a;
+	Atom a, p;
+
+	p = sym_table;
+
+	/*
+	 * Keep track of all symbols created
+	 * and return the same created atom
+	 * if found.
+	 */
+	while (!nilc(p))
+	{
+		a = car(p);
+		if (strcmp(a.symbol, value) == 0)
+			return a;
+
+		p = cdr(p);
+	}
+
+	/*
+	 * If we have not found the symbol
+	 * then it means we have a new one
+	 * so we  must create it and store it.
+	 */
 	a.type = AtomType_Symbol;
 	a.symbol = _strdup(value);
 
+	sym_table = cons(a, sym_table);
+
+
 	return a;
+}
+
+/* Read a single object and return error status and a pointer to the reminder of the input. */
+static int read_expr(const char* input, const char** end, Atom* result);
+
+/*
+ * Returns the start and the end of the next token in a string
+ * eg: (foo bar) -> |(| |foo| |bar| |)| , 4 different tokens
+ */
+static int lexer(const char* str, const char** start, const char** end)
+{
+	const char* ws = " \t\n";
+	const char* delim = " ()\t\n";
+	const char* prefix = "()";
+
+	str += strspn(str, ws);
+	/*
+	 * In the example from above str[0] will be:
+	 * (
+	 * f
+	 * f
+	 * b
+	 * b
+	 * )
+	 */
+
+
+	/*
+	 * If it hits the end of a string
+	 * without finding a token return a syntax error.
+	 */
+	if (str[0] == '\0')
+	{
+		*start = *end = NULL;
+		return Error_Syntax;
+	}
+
+	*start = str;
+
+	/*
+	 * Look for () and return what comes after ( or )
+	 */
+	if (strchr(prefix, str[0]) != NULL)
+	{
+		*end = str + 1;
+	}
+	else
+	{
+		*end = str + strcspn(str, delim);
+	}
+
+	return Error_OK;
+}
+
+static int parse_simple(const char* start, const char* end, Atom* result)
+{
+
+	char *buf, *p;
+
+	/* Integer? */
+	long val = strtol(start, &p, 10);
+
+	if (p == end)
+	{
+		result->type = AtomType_Integer;
+		result->integer = val;
+
+		return Error_OK;
+	}
+
+	/* NIL or symbol */
+	buf = malloc(sizeof(end - start + 1));
+
+	p = buf;
+
+	while (start != end)
+	{
+		*p++ = toupper(*start);
+		++start;
+	}
+	*p = '\0';
+
+	if (strcmp(buf, "NIL") == 0)
+		*result = nil;
+	else
+		*result = make_symbol(buf);
+
+	free(buf);
+
+	return Error_OK;
+}
+
+static int read_list(const char* start, const char** end, Atom* result)
+{
+	Atom p;
+
+	*end = start;
+	p = *result = nil;
+
+	for (;;)
+	{
+		const char* token;
+		Atom item;
+		Error err;
+
+		err = lexer(*end, &token, end);
+		if (err)
+			return err;
+
+		if (token[0] == ')')
+			return Error_OK;
+
+		if (token[0] == '.' && *end - token == 1)
+		{
+			/* Improper list */
+			if (nilc(p))
+				return Error_Syntax;
+
+			err = read_expr(*end, end, &item);
+			if (err)
+				return err;
+
+			cdr(p) = item;
+
+			/* Read the closing ')' */
+			err = lexer(*end, &token, end);
+			if (!err && token[0] != ')')
+				err = Error_Syntax;
+
+			return err;
+		}
+
+		err = read_expr(token, end, &item);
+		if (err)
+			return err;
+
+		if (nilc(p))
+		{
+			/* First item */
+			*result = cons(item, nil);
+			p = *result;
+		}
+		else
+		{
+			cdr(p) = cons(item, nil);
+			p = cdr(p);
+		}
+
+	}
+
+	return Error_OK;
+}
+
+static int read_expr(const char* input, const char** end, Atom* result)
+{
+	const char* token;
+	Error err;
+
+	err = lexer(input, &token, end);
+	if (err)
+		return err;
+
+	if (token[0] == '(')
+		return read_list(*end, end, result);
+	else if (token[0] == ')')
+		return Error_Syntax;
+	else
+		return parse_simple(token, *end, result);
 }
 
 static void print_expr(Atom atom)
@@ -92,17 +297,17 @@ static void print_expr(Atom atom)
 
 		case AtomType_Pair:
 			putchar('(');
-			printf("\n");
 			print_expr(car(atom));
 			atom = cdr(atom);
 
 			while(!nilc(atom))
 			{
 				if (atom.type == AtomType_Pair)
-					{
-						print_expr(car(atom));
-						atom = cdr(atom);
-					}
+				{
+					putchar(' ');
+					print_expr(car(atom));
+					atom = cdr(atom);
+				}
 				else
 				{
 					printf(" . ");
@@ -117,7 +322,6 @@ static void print_expr(Atom atom)
 		default:
 			break;
 	}
-	printf("\n");
 }
 
 int main(int argc, char* argv[])
@@ -142,6 +346,29 @@ int main(int argc, char* argv[])
 			)));
 	print_expr(a);
 	*/
+
+	char buffer[1024];
+	while (fgets(buffer, 1024, stdin) != NULL)
+	{
+		Error err;
+		Atom a;
+
+		const char* p = buffer;
+
+		err = read_expr(p, &p, &a);
+
+		switch(err)
+		{
+			case Error_OK:
+				print_expr(a);
+				putchar('\n');
+				break;
+			case Error_Syntax:
+				puts("Syntax error!");
+				break;
+		}
+
+	}
 
 	return EXIT_SUCCESS;
 }
